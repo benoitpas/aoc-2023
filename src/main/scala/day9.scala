f542f82ee6c1ecb106c2258d883fccc5b5b29ca8
benoitpas <bpasquereau@gmail.com>
2023-12-10 20:12:30 +0000

    typo+use LazyList instead of Stream

--- src/main/scala/day9.scala
+++ src/main/scala/day9.scala
@@ -6,14 +6,14 @@ object Day9 extends ZIOAppDefault {
 
   def nextMeasure(measureStr: String) =
     val measure = measureStr.split(' ').map(_.toLong).toList
-    val derivations = Stream.iterate(measure)(m => (m.tail zip m).map(_ - _)).takeWhile(_.map(math.abs).sum > 0).toList
+    val derivations = LazyList.iterate(measure)(m => (m.tail zip m).map(_ - _)).takeWhile(_.map(math.abs).sum > 0).toList
     derivations.foldLeft(0L)((a, m) => a + m.last)
 
   def part1(report: List[String]) = report.map(nextMeasure).sum
 
   def run =
     for {
-      v <- Day1.readFile("day9_input2.txt")
+      v <- Day1.readFile("day9_input.txt")
       _ <- printLine(s"part1=${part1(v)}")
     } yield ()
 }
